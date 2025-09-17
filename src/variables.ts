4d78e277a3ce0a44b4319048a1ba1b9c62aa7a50
Alex Lindemann <AlexDeadmanLindemann@gmail.com>
2025-09-17 12:44:36 -0500

    Updated request url

--- src/variables.ts
+++ src/variables.ts
@@ -1,2 +1,2 @@
-export const REST_URL = "http://localhost:3000";
-// export const REST_URL = "https://react-render-example-9064c021e554.herokuapp.com";
+// export const REST_URL = "http://localhost:3000";
+export const REST_URL = "https://react-render-example-9064c021e554.herokuapp.com";
