79e56b29b06d823c9caa4513a84a77fd419c7f8c
Alex Lindemann <AlexDeadmanLindemann@gmail.com>
2025-09-25 10:56:30 -0500

    Button update

--- src/RenderExamples/ContextVsRedux/ContextVsReduxRoot.tsx
+++ src/RenderExamples/ContextVsRedux/ContextVsReduxRoot.tsx
@@ -8,13 +8,9 @@ const ContextVsReduxRoot: React.FC = () => {
     const [currentVersion, setCurrentVersion] = useState('context')
     return (
         <Box style={{ border: 'thin solid #5151d1', margin: 2, padding: 2 }} sx={{ padding: 2 }}>
-            <Button variant='contained' onClick={() => {
-                if (currentVersion === 'context') {
-                    setCurrentVersion('redux');
-                } else {
-                    setCurrentVersion('context')
-                }
-            }}>Switch Versions</Button>
+            
+                        <Button variant='contained' style={{margin: 2}} onClick={() => {setCurrentVersion('context')}}>Context Versions</Button>
+                        <Button variant='contained' style={{margin: 2}}  onClick={() => {setCurrentVersion('redux')}}>Redux Versions</Button>
             {currentVersion === 'context' && <ContextParent />}
             {currentVersion === 'redux' && <ReduxParent />}
         </Box>
